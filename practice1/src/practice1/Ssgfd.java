502b5374c6dc92af14a9e2dd6c519f091e9334f1
ssuta <ssuta@TUFGAMING-SHUBHAM-18>
2023-02-22 20:57:51 +0530

    jkgv

--- practice1/src/practice1/Ssgfd.java
+++ practice1/src/practice1/Ssgfd.java
@@ -5,6 +5,9 @@ public class Ssgfd {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("fxghfg");
+		System.out.println("fxghfg");
+		System.out.println("fxghfg");
+		System.out.println("fxghfg");
 
 	}
 
